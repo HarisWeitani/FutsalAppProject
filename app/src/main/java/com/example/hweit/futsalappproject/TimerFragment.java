e7296f99807ab291086c3e6306a3a0334faaa4f5
Haris Weitani <hweitani@gmail.com>
2017-12-24 17:09:16 +0700

    add timer belum bisa run di background belum di percantik

--- app/src/main/java/com/example/hweit/futsalappproject/TimerFragment.java
+++ app/src/main/java/com/example/hweit/futsalappproject/TimerFragment.java
@@ -1,18 +1,25 @@
 package com.example.hweit.futsalappproject;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by hweit on 12/23/2017.
  */
 
 public class TimerFragment extends Fragment {
+
+
 
     @Nullable
     @Override
@@ -24,11 +31,36 @@ public class TimerFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        final TextView timerText = (TextView) view.findViewById(R.id.txtTimer);
+
         view.findViewById(R.id.btnTimerTesting).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getActivity(), "Timer Testing", Toast.LENGTH_SHORT).show();
+
+                timerController(view,timerText);
+
             }
         });
+    }
+
+    public void timerController(View view, final TextView timerText){
+
+        final Button timerBtn = (Button) view.findViewById(R.id.btnTimerTesting);
+
+        new CountDownTimer(100000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                timerBtn.setVisibility(View.GONE);
+                int seconds = (int) (millisUntilFinished / 1000);
+                int minutes = seconds / 60;
+                seconds = seconds % 60;
+                timerText.setText(String.format("%d:%02d", minutes, seconds));
+            }
+            public void onFinish() {
+                timerText.setText("done!");
+                timerBtn.setVisibility(View.VISIBLE);
+            }
+        }.start();
+
     }
 }
